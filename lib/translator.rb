08672fd5737d4737cf058718d3b4340512e158a6
Daniel Succar <danielsuccar@icloud.com>
2020-02-20 14:47:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,15 +16,33 @@ def load_library(file_path)
         'get_emoticon' => {}
       }
 
-      emoticons.each do |key,value|
-        new_hash['get_emoticon'][value[0]] = emoticons[key][1]
+        emoticons.each do |key,value|
+
+        new_hash['get_emoticon'][value[0]] = value[1]
         new_hash['get_meaning'][value[1]] = key
 
       end
+
       new_hash
+  #     {"get_meaning"=>
+  # {"☜(⌒▽⌒)☞"=>"angel",
+  #  "ヽ(ｏ`皿′ｏ)ﾉ"=>"angry",
+  #  "(ΘεΘ;)"=>"bored",
+  # "get_emoticon" =>
+  # {"0:)"=> (Japaneseemoji)
+  # ...}
 
 end
-
+#  emoticons[key][1]
+# => "☜(⌒▽⌒)☞"
+#  key
+# => "angel"
+#  emoticons[key]
+# => ["O:)", "☜(⌒▽⌒)☞"]
+# [value[0]]
+# => ["O:)"]
+# [value[1]]
+# => ["☜(⌒▽⌒)☞"]
 
 def get_japanese_emoticon(file_path, emoticon)
   result = load_library(file_path)['get_emoticon'][emoticon]
